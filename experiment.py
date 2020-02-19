8ebf2738d3499aad92b3a6ffe6e8b6f42bb20312
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-02-19 22:57:49 +0000

    an exercise in error suppression

---
+++ experiment.py
@@ -0,0 +1,85 @@
+from __future__ import absolute_import, division, print_function
+
+from os import abort
+
+from os import access # noqa
+
+from os import altsep # noqa: F401
+
+from os import chdir # lgtm
+
+from os import chmod # lgtm # noqa
+
+from os import chown # lgtm; noqa
+
+from os import chroot # lgtm # noqa: F401
+
+from os import close # lgtm; noqa: F401
+
+from os import closerange # noqa # lgtm
+
+from os import confstr # noqa; lgtm
+
+from os import confstr_names # noqa: F401 # lgtm
+
+from os import ctermid # noqa: F401; lgtm
+
+from os import (
+    ctermid,
+    curdir,
+)
+
+from os import ( # noqa
+    defpath,
+    devnull,
+)
+
+from os import ( # noqa: F401
+    dup,
+    dup2,
+)
+
+from os import ( # lgtm
+    environ,
+    errno,
+)
+
+from os import ( # lgtm # noqa
+    execl,
+    execle,
+)
+
+from os import ( # lgtm; noqa
+    execlp,
+    execlpe,
+)
+
+from os import ( # lgtm # noqa: F401
+    execv,
+    execve,
+)
+
+from os import ( # lgtm; noqa: F401
+    execvpe,
+    extsep,
+)
+
+from os import ( # noqa # lgtm
+    fchdir,
+    fchmod,
+)
+
+from os import ( # noqa; lgtm
+    fchown,
+    fdatasync,
+)
+
+from os import ( # noqa: F401 # lgtm
+    fdopen,
+    fork,
+)
+
+from os import ( # noqa: F401; lgtm
+    forkpty,
+    fpathconf,
+)
